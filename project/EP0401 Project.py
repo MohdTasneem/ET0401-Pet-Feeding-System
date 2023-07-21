7edd1f41ff140a6b634db3a75639e2f4094db1d2
Travis <travis05leow@gmail.com>
2023-07-21 14:50:06 +0800

    yes

--- project/EP0401 Project.py
+++ project/EP0401 Project.py
@@ -9,9 +9,9 @@ LCD = I2C_LCD_driver.lcd() #instantiate an lcd object, call it LCD
 
 def keypad():
     MATRIX = [[1, 2, 3],
-            [4, 5, 6],
-            [7, 8, 9],
-            ['*', 0, '#']]  # layout of keys on keypad
+              [4, 5, 6],
+              [7, 8, 9],
+              ['*', 0, '#']]  # layout of keys on keypad
     ROW = [6, 20, 19, 13]  # row pins
     COL = [12, 5, 16]  # column pins
 
@@ -24,22 +24,27 @@ def keypad():
     for j in range(4):
         GPIO.setup(ROW[j], GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+    # List to store the pressed keys
+    pressed_keys = []
+
     # scan keypad
     while (True):
         for i in range(3):  # loop thru’ all columns
             GPIO.output(COL[i], 0)  # pull one column pin low
             for j in range(4):  # check which row pin becomes low
                 if GPIO.input(ROW[j]) == 0:  # if a key is pressed
-                    print(MATRIX[j][i])  # print the key pressed
+                    pressed_keys.append(MATRIX[j][i])  # store the key pressed
                     while GPIO.input(ROW[j]) == 0:  # debounce
                         sleep(0.1)
             GPIO.output(COL[i], 1)  # write back default value of 1
 
+        # If any keys were pressed, return the result and clear the list
+        if pressed_keys:
+            result = "".join(str(key) for key in pressed_keys)
+            pressed_keys.clear()
+            return result    
+
 def display_led(string_to_display):
-    sleep(0.5)
-    LCD.backlight(0) #turn backlight off
-    sleep(0.5)
-    LCD.backlight(1) #turn backlight on 
     LCD.lcd_display_string(string_to_display, 1) #write on line 1
     # LCD.lcd_display_string("Address = 0x27", 2, 2) #write on line 2
     #             #starting on 3rd column
@@ -48,5 +53,11 @@ def clear_led():
     LCD.lcd_clear()
 
 def main():
-    display_led("Test")
+    LCD.lcd_display_string("Test", 1)
+    weight_input_value = keypad()
+    if (weight_input_value):
+        LCD.lcd_clear()
+        LCD.lcd_display_string("Weight: " + weight_input_value, 1)
+    
 
+main()

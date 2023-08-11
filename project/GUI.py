cce5044081c0559998d986a700d1a4c81475d351
Travis <travis05leow@gmail.com>
2023-08-11 14:34:32 +0800

    comments

--- project/GUI.py
+++ project/GUI.py
@@ -4,11 +4,13 @@ from tkinter import messagebox
 from tkinter import ttk
 from PIL import ImageTk, Image
 
+# Function to submit the phone number
 def submit_phone_number():
     phone_number = phone_entry.get()
     if phone_number:
         display_user_data(phone_number)
-        
+
+# Function to display the user's data in a table
 def display_user_data(phone_number):
     try:
         with open('data.csv', mode='r') as file:
@@ -22,10 +24,12 @@ def display_user_data(phone_number):
     except FileNotFoundError:
         messagebox.showerror("Error", "CSV file not found.")
 
+# Function to create the table
 def create_table(user_data):
-    table_frame = Frame(window)
+    table_frame = Frame(window) # Create a frame to hold the table
     table_frame.pack(padx=10, pady=10)
 
+    # Setting the header and columns of the table
     table = ttk.Treeview(table_frame, columns=('weight', 'success', 'time'))
     table.heading('#0', text='Phone Number')
     table.heading('weight', text='Weight (g)')
@@ -39,6 +43,7 @@ def create_table(user_data):
 
     table.pack()
 
+    # Inserting data into the table
     for data in user_data:
         phone_number = data['phone_number']
         weight = data['weight']
@@ -47,10 +52,12 @@ def create_table(user_data):
         
         table.insert('', 'end', text=phone_number, values=(weight, success, time_created))
 
+# Create the window
 window = Tk()
 window.title("Smart Paws GUI")
 window.minsize(width=800, height=500)
 
+# Setting up the background image
 frame = Frame(window, width=200, height=400)
 frame.pack()
 frame.place(anchor='center', relx=0.5, rely=0.5)
@@ -58,15 +65,19 @@ img = Image.open("Walter_White_S5B.png")
 img = img.resize((800, 200), Image.ANTIALIAS)
 img = ImageTk.PhotoImage(img)
 
+# Displaying the background image
 label = Label(frame, image = img)
 label.pack()
 
+# Title Label
 title_label = Label(window, text="Smart Paws GUI", font=("Arial", 16))
 title_label.pack(pady=5)
 
+# Description Label
 description_label = Label(window, text="Check your feeding history!", font=("Arial", 12))
 description_label.pack(pady=5)
 
+# Phone Number Label
 phone_label = Label(window, text="Enter your phone number:")
 phone_label.pack(pady=5)
 

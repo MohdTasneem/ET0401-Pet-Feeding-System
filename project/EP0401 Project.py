3dfbb96e3a4545610309ca3cfabfe1cc65d8ee69
Travis <travis05leow@gmail.com>
2023-08-11 14:29:03 +0800

    comments

--- project/EP0401 Project.py
+++ project/EP0401 Project.py
@@ -13,9 +13,11 @@ from imgurpython import ImgurClient
 import datetime
 import csv
 
+# Initialize GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
+# Initialize the camera
 camera = PiCamera()
 camera.start_preview()
 
@@ -23,6 +25,7 @@ LCD = I2C_LCD_driver.lcd()
 GPIO.setup(26,GPIO.OUT)
 PWM = GPIO.PWM(26, 50)
 
+# Keypad setup
 MATRIX = [[1, 2, 3],
           [4, 5, 6],
           [7, 8, 9],
@@ -39,12 +42,14 @@ for i in range(3):
 for j in range(4):
     GPIO.setup(ROW[j], GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+# Function to write data to data.csv
 def write_to_csv(phone_number, weight, success):
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     with open('data.csv', 'a') as file:
         writer = csv.writer(file, delimiter = ',', lineterminator = '\n')
         writer.writerow([phone_number, weight, success, timestamp])
 
+# Function to upload image to imgur and return the url
 def upload_to_imgur(path):
     client_id = '150fcc365051925'
     client_secret = '4dd7a56dd94eaa89ed6925f0489d50bbec5fab38'
@@ -53,19 +58,20 @@ def upload_to_imgur(path):
     response = client.upload_from_path(path, config=None, anon=True)
     return response['link']
 
+# Function to send text message
 def send_text_message(destination, message, image_url):
     try:
         account_sid = 'AC17f0a9ec890036265cba68687f70a297'
         auth_token = '75f2310a56e63c08a3738b16caf2d16d'
-        client = Client(account_sid, auth_token)
+        client = Client(account_sid, auth_token) # Initialize Twilio client
         print(image_url)
-        if (image_url == None):
+        if (image_url == None): # If no image is provided, send a text message
             message = client.messages.create(
                 to=destination,
                 from_='+12185629896',
                 body=message,
             )
-        else:
+        else: # If an image is provided, send a MMS
             message = client.messages.create(
                 from_='+12185629896',
                 to=destination,
@@ -78,6 +84,7 @@ def send_text_message(destination, message, image_url):
         print(err)
         return err.status
 
+# Function to format time input to 24 hour format
 def format_to_24_hour(time_input):
     if len(time_input) >= 4:
         hours = time_input[:2]
@@ -88,9 +95,11 @@ def format_to_24_hour(time_input):
     else:
         return "Invalid time format"
 
+# Function to get current time
 def get_current_time():
     return time.strftime("%H:%M:%S")
 
+# Function to update the time display on the LED
 def update_time_display(time_input):
     while True:
         LCD.lcd_display_string("To feed " + time_input, 1)
@@ -98,14 +107,16 @@ def update_time_display(time_input):
         LCD.lcd_display_string("Time " + current_time, 2)
         time.sleep(1)
 
+# Function to take a picture using the camera module
 def take_picture(path):
         camera.capture(path)
         img = Image.open(path)
 
+# Function to get input from the keypad
 def get_keypad_input(title):
     pressed_keys = ""
     while True:
-        if "(24hr)" in title:
+        if "(24hr)" in title: # If the title contains (24hr), format the time input
             formatted_time = pressed_keys[:2] + ":" + pressed_keys[2:]
             LCD.lcd_display_string(title + formatted_time, 2)
         else:
@@ -115,11 +126,11 @@ def get_keypad_input(title):
             GPIO.output(COL[i], 0)  # pull one column pin low
             for j in range(4):  # check which row pin becomes low
                 if GPIO.input(ROW[j]) == 0:  # if a key is pressed
-                    if MATRIX[j][i] == "*":
+                    if MATRIX[j][i] == "*": # If the * key is pressed, delete the last character
                         pressed_keys = pressed_keys[:-1]
                         print(pressed_keys)
                         LCD.lcd_display_string(title + "            " + pressed_keys, 2)
-                    elif MATRIX[j][i] == "#":
+                    elif MATRIX[j][i] == "#": # If the # key is pressed, submit the input
                         submit_pressed = True
                     else:
                         print(MATRIX[j][i])  # print the key pressed
@@ -132,79 +143,81 @@ def get_keypad_input(title):
             return pressed_keys
 
 
+# Function to play the buzzer
 def play_buzzer():
     GPIO.setup(18,GPIO.OUT)
     GPIO.output(18, 1)
     time.sleep(1)
     GPIO.output(18, 0)
 
+# Main function
 def main():
     PWM.start(12) # close the dispenser
-    LCD.lcd_display_string("Start of setup", 1)
+    LCD.lcd_display_string("Start of setup", 1) # Tells user that the setup is starting
     time.sleep(1)
     LCD.lcd_clear()
 
-    LCD.lcd_display_string("Enter weight", 1)
-    weight = int(get_keypad_input("(g): "))
+    LCD.lcd_display_string("Enter weight", 1) # Asks user to enter the weight of the food
+    weight = int(get_keypad_input("(g): ")) # Gets the weight of the food from keypad input
     LCD.lcd_clear()
-    LCD.lcd_display_string("Enter time", 1)
-    feeding_time = get_keypad_input("(24hr): ")
-    feeding_time = format_to_24_hour(feeding_time)
+    LCD.lcd_display_string("Enter time", 1) # Asks user to enter the time of feeding in 24 hour format
+    feeding_time = get_keypad_input("(24hr): ") # Gets the time of feeding from keypad input
+    feeding_time = format_to_24_hour(feeding_time) # Formats the time of feeding to 24 hour format
     LCD.lcd_clear()
     
-    # Link phone number loop
+    # Loop to prompt the user if they would like to link their phone number
     while True:
-        LCD.lcd_display_string("Link phone no.?", 1)
-        link_number = int(get_keypad_input("0=no, 1=yes: "))
+        LCD.lcd_display_string("Link phone no.?", 1) # Asks user if they would like to link their phone number
+        link_number = int(get_keypad_input("0=no, 1=yes: ")) # Gets the user's choice from keypad input
         print(link_number)
         if link_number == 1: # If the user chooses to link the phone number, ask for opt to verify the phone number
             LCD.lcd_clear()
-            LCD.lcd_display_string("Enter phone no.", 1)
-            phone_number_input = get_keypad_input("")
+            LCD.lcd_display_string("Enter phone no.", 1) # Asks user to enter their phone number
+            phone_number_input = get_keypad_input("") # Gets the phone number from keypad input
             LCD.lcd_clear()
-            opt = random.randint(1000, 9999)
-            result = send_text_message("+65" + phone_number_input, "Your OTP is " + str(opt), None)
-            if result == 400:
+            opt = random.randint(1000, 9999) # Generates a random 4 digit OTP
+            result = send_text_message("+65" + phone_number_input, "Your OTP is " + str(opt), None) # Sends the OTP to the user's phone number
+            if result == 400: # If the phone number is invalid, prompt the user to enter a valid phone number
                 LCD.lcd_clear()
                 LCD.lcd_display_string("Invalid phone no.", 1)
                 time.sleep(1)
                 LCD.lcd_clear()
-            else:
-                LCD.lcd_display_string("OTP sent to", 1)
-                LCD.lcd_display_string("+65" + phone_number_input, 2)
+            else: # If the phone number is valid, prompt the user to enter the OTP
+                LCD.lcd_display_string("OTP sent to", 1) # Tells the user that the OTP has been sent to their phone number
+                LCD.lcd_display_string("+65" + phone_number_input, 2) # Displays the phone number that the OTP has been sent to
                 time.sleep(2)
                 LCD.lcd_clear()
-                LCD.lcd_display_string("Enter OTP", 1)
-                opt_input = get_keypad_input("")
-                while opt_input != str(opt):
+                LCD.lcd_display_string("Enter OTP", 1) # Asks user to enter the OTP
+                opt_input = get_keypad_input("") # Gets the OTP from keypad input
+                while opt_input != str(opt): # If the OTP entered is incorrect, prompt the user to enter the OTP again
                     LCD.lcd_clear()
                     LCD.lcd_display_string("Invalid OTP", 1)
                     LCD.lcd_display_string("Enter OTP", 2)
                     opt_input = get_keypad_input("")
                 LCD.lcd_clear()
-                LCD.lcd_display_string("Phone no. linked!", 1)
+                LCD.lcd_display_string("Phone no. linked!", 1) # Tells the user that the phone number has been linked
                 break  # Exit the loop if the phone number is valid
         else:
             break  # Exit the loop if the user chooses not to link the phone number
 
     LCD.lcd_clear()
-    LCD.lcd_display_string("Setup done!", 1)
+    LCD.lcd_display_string("Setup done!", 1) # Tells the user that the setup is done
     time.sleep(1)
     LCD.lcd_clear()
-    time_thread = threading.Thread(target=update_time_display, args=(feeding_time,))
-    time_thread.start()
-    while True:
+    time_thread = threading.Thread(target=update_time_display, args=(feeding_time,)) # Create a thread to update the time display in real time
+    time_thread.start() # Start the thread
+    while True: # Loop to check if the current time is the same as the feeding time
         current_time = get_current_time()
         print("current_time: " + current_time)
         print("feeding_time: " + feeding_time)
         print(current_time == feeding_time)
-        if current_time == feeding_time:
-            play_buzzer()
+        if current_time == feeding_time: # If the current time is the same as the feeding time, start the pet feeding process
+            play_buzzer() # Play the buzzer to simulate a speaker playing a sound to get the pet's attention
             PWM.start(3)
             time.sleep(2)
             PWM.start(12)
             
-            if link_number == 1:
+            if link_number == 1: # If the user has linked their phone number, send a text message to the user
                 path = '/home/pi/Desktop/ET0401-Pet-Feeding-System/project/pet.jpg'
                 take_picture(path)
                 image_url = upload_to_imgur(path)
